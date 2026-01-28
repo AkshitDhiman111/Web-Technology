f2ffe290ed51b95ce53d3fe70f766561c9aae395
AkshitDhiman111 <dhimanakshit111@gmail.com>
2026-01-28 15:57:58 +0530

    code

---
+++ Argument function.js
@@ -0,0 +1,5 @@
+let score = 10
+score += 5
+score *= 3
+score -= 10
+console.log(score)
---
+++ arrow.js
@@ -0,0 +1,8 @@
+const func = (name) => {
+    return `This is ${name} function`
+}
+let result = func("Akshit")
+console.log(result)
+
+let result2 = func("Abhishek")
+console.log(result2)
